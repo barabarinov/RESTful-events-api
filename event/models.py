a6d71dd289d7f82d17a2f4896bf1875892167449
barabarinov <barabarinov@gmail.com>
2024-12-15 12:23:14 +0200

    Added guests field to Event model

--- event/models.py
+++ event/models.py
@@ -10,6 +10,9 @@ class Event(models.Model):
     organizer = models.ForeignKey(
         settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="events"
     )
- 
+    guests = models.ManyToManyField(
+        settings.AUTH_USER_MODEL, related_name="guest_events", blank=True
+    )
+
     def __str__(self):
         return self.title
