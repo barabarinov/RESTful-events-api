8f5121d46862274161edf2b4a070147c9e6179c3
barabarinov <barabarinov@gmail.com>
2024-12-15 18:42:38 +0200

    Changed field name date to datetime

--- event/models.py
+++ event/models.py
@@ -5,7 +5,7 @@ from django.db import models
 class Event(models.Model):
     title = models.CharField(max_length=200)
     description = models.TextField()
-    date = models.DateTimeField()
+    datetime = models.DateTimeField()
     location = models.CharField(max_length=255)
     organizer = models.ForeignKey(
         settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="events"
