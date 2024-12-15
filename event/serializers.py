08e78e894f62b9b462365ca9f7b249eeebe9e618
barabarinov <barabarinov@gmail.com>
2024-12-15 12:24:18 +0200

    Added guests field to EventSerializer

--- event/serializers.py
+++ event/serializers.py
@@ -1,11 +1,15 @@
+from django.contrib.auth import get_user_model
 from rest_framework import serializers
 
 from .models import Event
 
 
 class EventSerializer(serializers.ModelSerializer):
+    guests = serializers.PrimaryKeyRelatedField(
+        many=True, queryset=get_user_model().objects.all(), required=False
+    )
+
     class Meta:
         model = Event
         fields = "__all__"
         read_only_fields = ("organizer",)
-        
