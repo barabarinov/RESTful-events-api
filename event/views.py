03117bb8c1c0594f8385d476dc956c7c196b210a
barabarinov <barabarinov@gmail.com>
2024-12-15 18:43:30 +0200

    Added swagger_auto_schema to EventViewSet class

--- event/views.py
+++ event/views.py
@@ -1,3 +1,5 @@
+from drf_yasg.utils import swagger_auto_schema
+from drf_yasg import openapi
 from rest_framework import status, viewsets
 from rest_framework.decorators import action
 from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly
@@ -9,12 +11,20 @@ from .serializers import EventSerializer
 
 
 class EventViewSet(viewsets.ModelViewSet):
-    queryset = Event.objects.all().order_by("-date")
+    queryset = Event.objects.all().order_by("-datetime")
     serializer_class = EventSerializer
     permission_classes = [IsAuthenticatedOrReadOnly, IsEventOrganizerOrReadOnly]
 
     def perform_create(self, serializer):
         serializer.save(organizer=self.request.user)
+
+    @swagger_auto_schema(
+        operation_description="Register the authenticated user for a specific event.",
+        responses={
+            200: openapi.Response("Successfully registered for the event."),
+            400: openapi.Response("You are already registered for this event."),
+        },
+    )
 
     @action(detail=True, methods=["post"], permission_classes=[IsAuthenticated])
     def register(self, request, pk=None):
