925fd6007c9dbf25715ad9d49fce32fdf68c6711
barabarinov <barabarinov@gmail.com>
2024-12-15 12:25:10 +0200

    Added register method for user registration on event

--- event/views.py
+++ event/views.py
@@ -1,5 +1,8 @@
-from rest_framework import viewsets
-from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from rest_framework import status, viewsets
+from rest_framework.decorators import action
+from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly
+from rest_framework.response import Response
+
 from .models import Event
 from .permissions import IsEventOrganizerOrReadOnly
 from .serializers import EventSerializer
@@ -12,3 +15,17 @@ class EventViewSet(viewsets.ModelViewSet):
 
     def perform_create(self, serializer):
         serializer.save(organizer=self.request.user)
+
+    @action(detail=True, methods=["post"], permission_classes=[IsAuthenticated])
+    def register(self, request, pk=None):
+        event = self.get_object()
+        if request.user in event.guests.all():
+            return Response(
+                {"detail": "You are already registered for this event."},
+                status=status.HTTP_400_BAD_REQUEST,
+            )
+        event.guests.add(request.user)
+        return Response(
+            {"detail": "Successfully registered for the event."},
+            status=status.HTTP_200_OK,
+        )
